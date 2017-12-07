a11471ee1a5ad8ea3b6f6b6dd1f0d5c038cd7c67
kevinchyun3502 <32549493+kevinchyun3502@users.noreply.github.com>
2017-12-07 14:53:37 -0500

    made it into class

--- Encryption/encyrption.py
+++ Encryption/encyrption.py
@@ -1,8 +1,20 @@
-message = str(input('Type in your message. This program will encrypt your message safely.'))
-changed = ''  
-c = ''
-for i in range(len(message)):
-	chr(ord(message[i])+2)
-	changed = changed + str(chr(ord(message[i])+2))
+class encryption:
+	def__init(self, message, shift)
+		self.message = message
+		self.shift = shift
+		self.message = 'hello'
+		self.shift = 2
+
+	def message(self,message):
+		message = self.message
+		if self.message = '':
+			self.message = 'hello'
+
+	def shift(self, message):
+		changed = ''  
 		
-print(changed)
+		for i in range(len(message)):
+			chr(ord(message[i])+self.shift)
+			changed = changed + str(chr(ord(message[i])+2))
+		
+		print(changed)
