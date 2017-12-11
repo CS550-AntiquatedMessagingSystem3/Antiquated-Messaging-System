2db27d7ea32408b8d772293bb8b14d0d744b26e1
siri-palreddy <34071747+siri-palreddy@users.noreply.github.com>
2017-12-11 08:03:08 -0500

    Create EncryptionShift.py

---
+++ FINAL/EncryptionShift.py
@@ -0,0 +1,30 @@
+import os
+
+class encryption:
+    def __init__(self, textData = 'hello', shiftNumber = 2):
+        self.textData = textData
+        self.shiftNumber = shiftNumber
+    
+    def message(self,textData):
+        self.textData = textData
+        if self.textData == '':
+            self.textData = 'hello'
+
+    def shift(self):
+        changed = ''  
+        for i in range(len(self.textData)):
+            changed = changed + str(chr(ord(self.textData[i])+self.shiftNumber))
+        output_filename = os.path.normpath("code.txt")    
+        file = open(output_filename,"a")
+        file.write(changed + '\n')
+        file.close() 
+        return(changed)
+
+    def decrypt(self):
+        changed = ''  
+        for i in range(len(self.textData)):
+            changed = changed + str(chr(ord(self.textData[i])-self.shiftNumber))
+        output_filename = os.path.normpath("decode.txt")    
+        file = open(output_filename,"a")
+        file.write(changed + '\n')
+        file.close() 
