eb3823c222c9d519fddd6c652776e4100b401c16
siri-palreddy <34071747+siri-palreddy@users.noreply.github.com>
2017-12-11 08:01:54 -0500

    Create MorseCodeSystemFinal.py

---
+++ FINAL/MorseCodeSystemFinal.py
@@ -0,0 +1,90 @@
+import os
+
+class Messenger:
+
+    morseAlphabet ={
+            "A" : ".-",
+            "B" : "-...",
+            "C" : "-.-.",
+            "D" : "-..",
+            "E" : ".",
+            "F" : "..-.",
+            "G" : "--.",
+            "H" : "....",
+            "I" : "..",
+            "J" : ".---",
+            "K" : "-.-",
+            "L" : ".-..",
+            "M" : "--",
+            "N" : "-.",
+            "O" : "---",
+            "P" : ".--.",
+            "Q" : "--.-",
+            "R" : ".-.",
+            "S" : "...",
+            "T" : "-",
+            "U" : "..-",
+            "V" : "...-",
+            "W" : ".--",
+            "X" : "-..-",
+            "Y" : "-.--",
+            "Z" : "--..",
+            " " : "/",
+            "1" : ".----",
+            "2" : "..---",
+            "3" : "...--",
+            "4" : "....-",
+            "5" : ".....",
+            "6" : "-....",
+            "7" : "--...",
+            "8" : "---..",
+            "9" : "----.",
+            "0" : "-----",
+            "." : ".-.-.-",
+            "," : "--..--",
+            ":" : "---...",
+            "?" : "..--..",
+            "'" : ".----.",
+            "-" : "-....-",
+            "/" : "-..-.",
+            "@" : ".--.-.",
+            "=" : "-...-"
+            }
+
+    inverseMorseAlphabet = dict((v, k) for (k, v) in morseAlphabet.items())
+
+    def encodeToMorse(textData):
+        encodedMessage = ""
+        for char in textData[:]:
+            encodedMessage += Messenger.morseAlphabet[char.upper()] + " "          
+        return encodedMessage
+    
+    def decodeFromMorse(textData):
+        decodedMessage = ""
+        textDataSeparated = textData.split(' ')
+        for char in textDataSeparated:
+            if char in Messenger.inverseMorseAlphabet:
+                decodedMessage += Messenger.inverseMorseAlphabet[char]
+            #else:
+             #   # CNF = Character not found
+              #  decodedMessage += '<CNF>'       
+        return decodedMessage
+
+class Transmitter:
+    
+    def decodeFromMorse(textData):
+        output_filename = os.path.normpath("decode.txt")    
+        decodeText = Messenger.decodeFromMorse(textData)
+        file = open(output_filename,"a")
+        file.write(decodeText + '\n')
+        file.close() 
+        
+    def encodeToMorse(textData):
+        output_filename = os.path.normpath("code.txt")    
+        encodeText = Messenger.encodeToMorse(textData)
+        file = open(output_filename,"a")
+        file.write(encodeText + '\n')
+        file.close() 
+
+
+
