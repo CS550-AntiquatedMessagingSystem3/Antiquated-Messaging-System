4e130af2a64e74929715e0aff3af813c8075d12e
siri-palreddy <34071747+siri-palreddy@users.noreply.github.com>
2017-12-10 21:33:09 -0500

    Morse Code Final

--- Morse-Code/Morse-Code.py
+++ Morse-Code/Morse-Code.py
@@ -2,25 +2,30 @@ import sys
 import re
 import xlsxwriter
 import codesystem
-
+import encyrption
 encodeChoice = 0
 
 while (encodeChoice < 1 or encodeChoice > 2):
     try:
         # Get encoded choice
-        encodeChoice = int(input('Messaging Code System:\n1: Encryption\n2: Morse Code\nEnter the number corresponding to your encoding choice?'))
+        encodeChoice = int(input('Messaging Code System:\n1: Encryption\n2: Morse Code\n3: Pigeon Carrier\nEnter the number corresponding to your encoding choice:'))
     except ValueError:
         # Make sure it is a valid number
-        print("That wasn't a proper choice")
+        print("That wasn't a proper choice.")
     
 
 textBlock = []
 while True:
-    sourceText = input('\nType text to encode one line at a time. Press enter to start a new line. Type Q in a separate line to end the message?')
+    sourceText = input('\nType text to encode one line at a time. Press enter to start a new line. Type Q in a separate line to end the message.')
     if sourceText.lower() == 'q':
         break
     textBlock.append(sourceText)
 
 for textData in textBlock:
-    codesystem.Transmitter.encodeToMorse(textData)
+    if encodeChoice == 2:
+        codesystem.Transmitter.encodeToMorse(textData)
+    if encodeChoice == 1:
+        encryption.encryption.shift(textData)
+    if encodeChouce == 3:
+
 
